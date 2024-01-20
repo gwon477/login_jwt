be7b39d44a49c7fb738edeaa8459712796be96ba
gwonsang247 <gwonsang247@gmail.com>
2024-01-21 00:37:53 +0900

    [TIL]expired_response

--- src/main/java/com/example/springjwt/jwt/JWTFilter.java
+++ src/main/java/com/example/springjwt/jwt/JWTFilter.java
@@ -1,8 +1,14 @@
 package com.example.springjwt.jwt;
 
+import com.example.springjwt.dto.CMResDto;
 import com.example.springjwt.dto.CustomUserDetails;
+import com.example.springjwt.dto.TokenResponseDto;
 import com.example.springjwt.entity.UserEntity;
 import com.example.springjwt.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +20,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Map;
 
 public class JWTFilter extends OncePerRequestFilter {
 
@@ -31,7 +39,7 @@ public class JWTFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         //request에서 Authorization 헤더를 찾음
-        String authorization= request.getHeader("Authorization");
+        String authorization = request.getHeader("Authorization");
 
         //Authorization 헤더 검증
         if (authorization == null || !authorization.startsWith("Bearer ")) {
@@ -42,24 +50,29 @@ public class JWTFilter extends OncePerRequestFilter {
             //조건이 해당되면 메소드 종료 (필수)
             return;
         }
-
+        // 헤더가 있기 때문에 헤더를 추출
         String token = authorization.split(" ")[1];
 
-        //토큰 소멸 시간 검증
-        if (jwtUtil.isExpired(token)) {
+        // 헤더가 만료되었는 확인
+        try {
+            jwtUtil.isExpired(token);
+        } catch (ExpiredJwtException e) {
 
             System.out.println("token expired");
-            filterChain.doFilter(request, response);
 
-            //조건이 해당되면 메소드 종료 (필수)
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            CMResDto<Void> cmRespDto = CMResDto.<Void>builder()
+                    .code(HttpServletResponse.SC_UNAUTHORIZED) // 401 Unauthorized
+                    .msg("토큰이 만료되었습니다.")
+                    .build();
+
+            writeResponse(response, cmRespDto);
+
+            //filterChain.doFilter(request, response);
             return;
         }
 
         String email = jwtUtil.getEmail(token);
-
-//        UserEntity userdata = userRepository.findByemail(email);
-//        String username = userdata.getUsername();
-        //String username = jwtUtil.getUsername(token);
         String role = jwtUtil.getRole(token);
 
         UserEntity userEntity = new UserEntity();
@@ -75,5 +88,31 @@ public class JWTFilter extends OncePerRequestFilter {
         SecurityContextHolder.getContext().setAuthentication(authToken);
 
         filterChain.doFilter(request, response);
+    }
+    // JSON 응답을 생성하는 메소드
+    private void writeResponse(HttpServletResponse response, CMResDto<?> cmRespDto) {
+        try {
+            // cmRespDto 객체로 변환해서 타입 반환.
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // cmRespDto 내부에 LocalDatetime 형식 변환 설정.
+            objectMapper.registerModule(new JavaTimeModule());
+            objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS"));
+
+            // response body에 담기.
+            String jsonResponse = objectMapper.writeValueAsString(cmRespDto);
+
+            // response 타입지정.
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+
+            // response 반환.
+            response.getWriter().write(jsonResponse);
+
+        } catch (IOException e) {
+            // 에러 핸들링
+            e.printStackTrace();
+        }
     }
 }
